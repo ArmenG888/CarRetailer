6e153a61e9a37da906e0ab3f1965b489786171fc
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2022-02-21 15:05:22 -0800

    Update models.py

--- CarRetailer/cars/models.py
+++ CarRetailer/cars/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class car(models.Model):
+    brand_name = models.CharField(default="")
+    model_name = models.CharField(default="")
+    year = models.IntegerField(default=2000)
+    mileage = models.IntegerField(defualt=10000)
